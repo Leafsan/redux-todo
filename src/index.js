94a966b7d609ee27904fa95c92d70f3fc1b5946b
Wookyung Choi <cwk0906@naver.com>
2023-06-24 04:47:12 +0900

    add, delete feature added. paint todos added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,74 @@
+import { createStore } from "redux";
 
+const form = document.querySelector("form");
+const ul = document.querySelector("ul");
+
+const input = document.getElementById("toDo");
+
+const Action = {
+  ADD: "ADD_TODO",
+  DELETE: "DELETE_TODO",
+};
+Object.freeze(Action);
+
+const addToDo = (text) => {
+  return {
+    type: Action.ADD,
+    text,
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: Action.DELETE,
+    id,
+  };
+};
+
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (event) => {
+  const id = parseInt(event.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case Action.ADD:
+      return [...state, { text: action.text, id: Date.now() }];
+    case Action.DELETE:
+      return state.filter((toDos) => toDos.id !== action.id);
+    default:
+      return;
+  }
+};
+
+const store = createStore(reducer);
+
+const paintToDos = () => {
+  ul.innerHTML = "";
+  const toDos = store.getState();
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
+
+const onSubmit = (event) => {
+  event.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddToDo(toDo);
+};
+
+form.addEventListener("submit", onSubmit);
